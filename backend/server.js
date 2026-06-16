cb0256a89a35161996d2282199c83c2f55b781b5
Prince4157 <200225016+Prince4157@users.noreply.github.com>
2026-06-16 11:21:10 +0530

    fix: update Express 5 catch-all route syntax to fix startup crash

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ app.get('/api/reports/volunteers', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
